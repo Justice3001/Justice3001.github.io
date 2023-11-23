b9c70ed118e84337456749e82f399b6c9ced6b86
Leonardo-Costa-9000 <randeste@outlook.com>
2023-11-23 14:20:56 -0500

    send Please

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,27 +1,28 @@
 // Import react into the bundle
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Include your index.scss file into the bundle
-import '../styles/index.css';
+import "../styles/index.css";
 
 // Import your own components
-import Layout from './layout';
-import Search from './pages/search.jsx';
-import Recommendations from './pages/Recon.jsx';
-import App from './pages/App.jsx';
+import Layout from "./layout";
+import Search from "./pages/search.jsx";
+import Recommendations from "./pages/Recon.jsx";
+import App from "./pages/App.jsx";
 
 // Import the SpotifyAuthWrapper component
-import SpotifyAuthWrapper from './component/SpotifyAuthWrapper.js';
+import SpotifyAuthWrapper from "./component/SpotifyAuthWrapper.js";
 
 // Render your react application wrapped in SpotifyAuthWrapper
 ReactDOM.render(
   <SpotifyAuthWrapper>
     {/* Your other components that require Spotify authentication */}
-     <Layout /> 
+    <Layout />
     {/* Example: <Search /> */}
-     {/*<Recommendations /> */}
+    {/*<Recommendations /> */}
     {/* Example: <App /> */}
   </SpotifyAuthWrapper>,
-  document.querySelector('#app')
+  document.querySelector("#app")
 );
+//this is nice
